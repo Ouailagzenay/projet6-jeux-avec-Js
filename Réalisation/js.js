d00d511fe727e98920c3dc79e37eabe97515e2e5
ouailagzenay <ouailagzenay@gmail.com>
2021-11-30 16:42:29 +0100

    update

--- Réalisation/js.js
+++ Réalisation/js.js
@@ -1,25 +1,42 @@
 // declaration des variables 
-var botn ;
-var output ;
-var number ;
-var nombreDeviner ;
-var att
+var number;
+var output;
+var botn;
+var attmpes;
+var nombreDeviner;
+// entrer: saiser
+botn= document.getElementById('botn');
+output=document.getElementById('outputText');
+number=Math.floor(Math.random() * 100);
+attmpes = 10 ;
+console.log(number)
+outputattmpes = document.getElementById('tentative')
+// traitement
+botn.addEventListener('click' , function(){
+attmpes--
+nombreDeviner=document.getElementById('userInput').value;
+if (attmpes > 0) {
+   outputattmpes.innerHTML= 'attmpes = '+attmpes 
+  if(nombreDeviner == number){
+    if(nombreDeviner == number && attmpes >=8){
+      output.innerHTML = 'TRES BEIN'
+      }else{
+      if(nombreDeviner == number && attmpes >=3){
+      output.innerHTML = 'bein'
+      }
+     }
 
- // entrer: saiser 
- botn = document.getElementById('botn');
- output = document.getElementById('outputText');
- number = Math.floor(Math.random() * 100);
- // traitement 
-   botn.addEventListener('click' , function(){
-    nombreDeviner=document.getElementById("userInput").value;
-   if ( nombreDeviner == number ){ 
-     output.innerHTML = "Félicitations" 
-   }else if (nombreDeviner > number) {
-    output.innerHTML = "c'est nomber est plus petit que" 
-   }
-   else {
-     output.innerHTML = "c'est nomber est plus grand que"
-   }
+    }else{
+    if(nombreDeviner > number){
+      output.innerHTML="c'est nomber est plus grand"
+    }else{
+      output.innerHTML="c'est nomber est plus petit"
+    }
+  }
+}else {
+    output.innerHTML = "c'est rate";
+    outputattmpes.innerHTML= "attmpes =" + 0;
   
- });
+}
+});
 
